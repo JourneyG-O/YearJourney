19260244faac00e41bdcda2971ec1aa225e6b3e7
JourneyG-O <gojs2639@gmail.com>
2025-11-20 17:20:38 +0900

    ✨ TabView 구조 생성

---
+++ YearJourney/YearJourney/Features/Themes/TemesView.swift
@@ -0,0 +1,18 @@
+//
+//  TemesView.swift
+//  YearJourney
+//
+//  Created by KoJeongseok on 11/20/25.
+//
+
+import SwiftUI
+
+struct TemesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemesView()
+}
---
+++ YearJourney/YearJourney/Features/Widgets/WidgetsView.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetsView.swift
+//  YearJourney
+//
+//  Created by KoJeongseok on 11/20/25.
+//
+
+import SwiftUI
+
+struct WidgetsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetsView()
+}
